ad3c7ab0057ef35723ba6bc621a8dbdc13bf313a
Rebo-rN <reborncrexx@gmail.com>
2026-03-16 18:44:52 +0545

    Add obfuscation and decoding functionality
    Added obfuscation comments and a lambda function for decoding.

--- Reborn.py
+++ Reborn.py
@@ -1,1 +1,6 @@
-hsh
+                    
+# Obfuscated with PyObfuscate
+# https://www.github.com/htr-tech
+# Time : Mon Mar 16 18:34:31 2026
+# -------------------------------
+_ = lambda __ : __import__('marshal').loads(__import__('base64').b64decode(__[::-1]));e>

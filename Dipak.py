93339811426a60d6eaf7862a1eb50d22869392e8
Rebo-rN <reborncrexx@gmail.com>
2026-03-29 22:13:32 +0545

    Remove obfuscated code and comments
    Removed obfuscated code and comments from Dipak.py.

--- Dipak.py
+++ Dipak.py
@@ -1,6 +1,1 @@
-# +++fuck55                    
-# Obfuscated with PyObfuscate
-# https://www.github.com/htr-tech
-# Time : Mon Mar 16 18:34:31 2026
-# -------------------------------
-_ = lambda __ : __import__('marshal').loads(__import__('base64').b64decode(__[::-1]))
+
